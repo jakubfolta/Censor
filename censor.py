79d6a7f2d362fb8b3b5ed6bdc4bb333c29334d8a
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-11-10 13:17:55 +0100

    another option

--- censor.py
+++ censor.py
@@ -1,6 +1,19 @@
 def censor(text, word):
   asterisksWord = len(word) * '*'
+  print(asterisksWord)
   return text.replace(word, asterisksWord)
+
+print(censor('abece dede sksi dede', 'dede'))
+
+
+
+def censor(text, word):
+  splittedText = text.split()
+  asterisksWord = len(word) * '*'
+  for index, position in enumerate(splittedText):
+    if position == word:
+      splittedText[index] = asterisksWord
+  return ' '.join(splittedText)
 
 print(censor('abece dede sksi dede', 'dede'))
 
