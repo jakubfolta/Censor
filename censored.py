0a752ebd43e76a81d333ca055239a57ac10c0871
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-11-18 09:47:38 +0100

    solidifying knowledge

---
+++ censored.py
@@ -0,0 +1,20 @@
+def censoredWord(text, word):
+  censored = len(word) * '*'
+  return text.replace(word, censored)
+
+print(censoredWord('abh dkkfir kvko keje kaka fnjeribv kaka', 'kaka'))
+
+
+def censor(text, word):
+  splittedText = text.split()
+  for index, each in enumerate(splittedText):
+    if each == word:
+      splittedText[index] = len(word) * '*'
+  return ' '.join(splittedText)
+
+print(censor('abh dkkfir kvko keje kaka fnjeribv kaka', 'kaka'))
+
+
+
+
+  
