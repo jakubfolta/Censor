62a1058892adf2fcc7b62482ed2844d01eff0e79
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-11 22:10:58 +0100

    solidifying knowledge

--- censored.py
+++ censored.py
@@ -17,4 +17,35 @@ print(censor('abh dkkfir kvko keje kaka fnjeribv kaka', 'kaka'))
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def replaceCensoredWord(text, word):
+  censored = len(word) * '*'
+  return text.replace(word, censored)
+
+print(replaceCensoredWord('abh dkkfir kvko keje kaka fnjeribv kaka', 'kaka'))
+
+
+
+
+
+
   
