b06b071d19827da67bf9fed97d390748c1b523da
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-21 15:24:28 +0200

    solidifying knowledge

--- censor_solidifying.py
+++ censor_solidifying.py
@@ -2,3 +2,14 @@ def censor(text, censored):
     return text.replace(censored, len(censored) * '*')
 
 print(censor('sadsad kala abhdhd ufhdjw kala fdsf kala', 'kala'))
+
+def censor(text, censor_word):
+    censor = len(censor_word) * '*'
+    text = text.split()
+
+    for index, x in enumerate(text):
+        if x == censor_word:
+            text[index] = censor
+            print(text)
+        #text[x] = censor if x == censor_word else text[x] = x
+print(censor('sadsad kala abhdhd ufhdjw kala fdsf kala', 'kala'))
